bdb4a7e8480feebfdefcaab63234c49463d35518
Михайлов Артём Андреевич <amihaylov@ask.local>
2020-09-02 16:04:04 +0700

    initial boilerplate and basic jest-enzyme test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow, cofigure, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='app-container']");
+
+  expect(appComponent.length).toBe(1);
 });
